29f5ba7b686c9e63ab76b86b1f24fe5e801b7883
albert <albertngcyfai@gmail.com>
2025-02-27 18:44:48 -0500

    Add preview feature to upload and package updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import ImageUploader from "./components/ImageUploader";
+import ImageHandler from "./components/ImageHandler";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-1 items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <ImageUploader />
+      <ImageHandler />
     </div>
   );
 }
