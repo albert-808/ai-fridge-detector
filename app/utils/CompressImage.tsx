208b61946eb18ee471849b07aa6eaa72fed4b2b1
albert <albertngcyfai@gmail.com>
2025-03-01 17:51:52 -0500

    Refactored prop type clarity

--- app/utils/CompressImage.tsx
+++ app/utils/CompressImage.tsx
@@ -1,6 +1,6 @@
 import imageCompression from "browser-image-compression";
 
-export default async function CompressImage(imageFile) {
+export default async function CompressImage(imageFile: File) {
   const options = {
     maxSizeMB: 0.02, // Target file size in MB
     maxWidthOrHeight: 600, // Maximum dimensions
@@ -17,10 +17,20 @@ export default async function CompressImage(imageFile) {
   }
 }
 
-async function convertFileToBase64(file) {
+async function convertFileToBase64(file: Blob) {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
-    reader.onloadend = () => resolve(reader.result.split(",")[1]); // Extract base64 part
+    reader.onloadend = () => {
+      if (reader.result) {
+        if (typeof reader.result === "string") {
+          resolve(reader.result.split(",")[1]); // Extract base64 part
+        } else {
+          reject(new Error("FileReader result is not a string"));
+        }
+      } else {
+        reject(new Error("FileReader result is null"));
+      }
+    };
     reader.onerror = reject;
     reader.readAsDataURL(file);
   });
