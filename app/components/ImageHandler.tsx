7664001b2941176b4b7b8eb2f7cb42afa79bc294
albert <albertngcyfai@gmail.com>
2025-03-02 01:36:57 -0500

    Feature added loading screen

--- app/components/ImageHandler.tsx
+++ app/components/ImageHandler.tsx
@@ -13,19 +13,29 @@ export default function ImageHandler() {
   const [resultOutput, setResultOutput] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [isGridVisible, setIsGridVisible] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isCompressing, setIsCompressing] = useState(false);
 
   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const imageFile = event.target.files ? event.target.files[0] : null;
 
     if (imageFile) {
+      setIsCompressing(true);
       const reader = new FileReader();
       reader.onload = async (e) => {
         if (e.target && e.target.result) {
-          const commpressedBase64Image = await CompressImage(imageFile);
-          setImagePreview({
-            forDisplay: e.target.result as string,
-            base64: commpressedBase64Image as string,
-          });
+          try {
+            const commpressedBase64Image = await CompressImage(imageFile);
+            setImagePreview({
+              forDisplay: e.target.result as string,
+              base64: commpressedBase64Image as string,
+            });
+          } catch (error) {
+            setErrorMessage(`Image compression error: ${error}`);
+            console.error("Image compression error:", error);
+          } finally {
+            setIsCompressing(false);
+          }
         }
       };
       reader.readAsDataURL(imageFile);
@@ -38,6 +48,7 @@ export default function ImageHandler() {
       setErrorMessage("No image found. Please upload an image.");
       return;
     }
+    setIsLoading(true);
     try {
       const response = await fetch("/api/scanFridgeImage", {
         method: "POST",
@@ -52,12 +63,16 @@ export default function ImageHandler() {
       if (!response.ok) {
         const error = await response.json();
         console.error("Error with analyzing image:", error);
+        setIsLoading(false);
+        return;
       }
       const result = await response.json();
       setResultOutput(result.output);
     } catch (error) {
       console.error("Error sending requset:", error);
       setErrorMessage(`Error with sending request: ${error}`);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -82,19 +97,30 @@ export default function ImageHandler() {
                 : "grid-cols-1"
             }
           >
-            <div className="bg-white backdrop-blur-sm p-8 w-[400px] text-center shadow-md rounded-md relative z-10 w-full h-full">
+            <div className="bg-white backdrop-blur-sm p-8 text-center shadow-md rounded-md relative z-10 w-[600px] h-full">
               <div className="bg-[#ffd700] text-black p-2 mb-4 shadow-md rounded-md w-full">
                 <h2 className="text-xl font-semibold">Picture Upload</h2>
               </div>
 
-              <p>
-                Upload a picture by drag and drop or click and browse. Cloick on
+              <p className="pb-4">
+                Upload a picture by drag and drop or click and browse. Click on
                 the SUBMIT button to have our AI analyze your fridge.
               </p>
 
+              {/* Preview uploaded image */}
+              {imagePreview && (
+                <div className="flex justify-center mt-4">
+                  <img
+                    src={imagePreview.forDisplay}
+                    alt="Preview"
+                    className="h-full w-full rounded-lg"
+                  />
+                </div>
+              )}
+
               <div
                 id="uploadArea"
-                className="border-2 border-dashed border-gray-300 p-4 mb-4 rounded-md cursor-pointer hover:border-gray-400 transition-colors duration-200"
+                className="border-2 border-dashed border-yellow-300 p-4 mb-4 rounded-md cursor-pointer hover:border-gray-400 transition-colors duration-200"
               >
                 <input
                   type="file"
@@ -103,22 +129,15 @@ export default function ImageHandler() {
                   className="hidden"
                   onChange={handleImageChange}
                 />
-
-                {/* Preview uploaded image */}
-                {imagePreview && (
-                  <div className="flex justify-center mt-4">
-                    <img
-                      src={imagePreview.forDisplay}
-                      alt="Preview"
-                      className="max-w-xs max-h-64 rounded-lg  pb-4"
-                    />
-                  </div>
-                )}
                 <label
                   htmlFor="fileInput"
                   className="bg-[#00bfff] hover:bg-blue-600 transition-colors duration-200 text-white rounded-md cursor-pointer px-4 py-2"
                 >
-                  Choose file..
+                  {isCompressing
+                    ? "Preparing Picture..."
+                    : imagePreview
+                    ? "Choose another file.."
+                    : "Choose file.."}
                 </label>
               </div>
 
@@ -128,12 +147,26 @@ export default function ImageHandler() {
                   className="bg-[#008000] hover:bg-green-300 transition-colors duration-200 text-white font-bold rounded w-full cursor-pointer py-2 px-4"
                   type="submit"
                   onClick={handleSubmit}
+                  disabled={isLoading}
                 >
-                  Submit
+                  {isLoading ? "Loading..." : "Submit"}
                 </button>
               )}
             </div>
-
+            {isLoading && (
+              <div className="flex justify-center items-center shadow-2xl">
+                <div className="p-8 rounded-lg font-bold text-center">
+                  <img
+                    src="/pizza_loader.gif"
+                    alt="Loading..."
+                    width={150}
+                    height={150}
+                    className="rounded-full"
+                  />
+                  <p>Loading...</p>
+                </div>
+              </div>
+            )}
             {resultOutput && (
               <div className="h-full">
                 <IngredientsRender result={resultOutput} />
